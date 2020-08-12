37af6e8aa36d68e816411f5267cd7c104773ca87
Billy Gunn, Jr <bgunnjr@gmail.com>
2020-08-12 23:54:19 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,25 +1,28 @@
 class Artist 
- 
- @@all = []
- 
- def initialized(name)
-   @name = name 
-   @@all << self 
- end 
- 
- def self.all
-   @@all
- end
- 
- def new_song(name, genre)
-   
- end
- 
- def song
 
- end
- 
- def genre
-  
- end 
-end     #class end 
+  attr_accessor :name
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def songs
+    Song.all.select {|song| song.artist == self}
+  end
+
+  def genres
+    songs.map {|song| song.genre}
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_song(song, genre)
+    Song.new(name, self, genre)
+  end
+
+
+end 
