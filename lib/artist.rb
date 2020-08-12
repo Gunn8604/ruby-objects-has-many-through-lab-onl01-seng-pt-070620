59e45e2ce88aab261af9f469f35277272e2c57f8
Billy Gunn, Jr <bgunnjr@gmail.com>
2020-08-12 23:48:28 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,25 @@
+class Artist 
+ 
+ @@all = []
+ 
+ def initialized(name)
+   @name = name 
+   @@all << self 
+ end 
+ 
+ def self.all
+   @@all
+ end
+ 
+ def new_song(name, genre)
+   
+ end
+ 
+ def song
+
+ end
+ 
+ def genre
+  
+ end 
+end     #class end 
